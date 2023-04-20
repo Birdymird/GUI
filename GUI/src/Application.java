a8c4951abb55ec509129dd873dd5293eb11b0a61
Birdymird <117263838+Birdymird@users.noreply.github.com>
2023-04-20 08:36:30 +0200

    Add files via upload

---
+++ GUI/src/Application.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Application {
+    public static void main(String[] args){
+        ArrayList<Object> User = new ArrayList<>();
+
+    }
+}
---
+++ GUI/src/UserManagement.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class UserManagement {
+
+    private JPanel panel1;
+    private JPasswordField passwordField1;
+    private JProgressBar progressBar1;
+    private JButton button1;
+    private JTextField textField1;
+}
